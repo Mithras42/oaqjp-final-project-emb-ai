619ec37e0e7d39cd6d1705275b4f5fea00aed867
Mithras42 <andyc@starsend.force9.co.uk>
2026-04-06 15:09:11 -0400

    6a

--- server.py
+++ server.py
@@ -1,31 +1,30 @@
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detection
 
-#Initiate the flask app 
-app = Flask( __name__ )
+# Initiate the flask app
+app = Flask(__name__)
 
 
 @app.route("/emotionDetector")
 def emotionDetector():
-    ''' This code receives the text from the HTML interface and 
-        runs emotion analysis over it using emotion_detection()
-        function. The output returns the emotion scores and the 
-        dominant emotion
-    '''
+    """This code receives the text from the HTML interface and
+    runs emotion analysis over it using emotion_detection()
+    function. The output returns the emotion scores and the
+    dominant emotion
+    """
     # Retrieve the text to analyze from the request arguments
-    text_to_analyze = request.args.get('textToAnalyze')
+    text_to_analyze = request.args.get("textToAnalyze")
 
-    # Pass the text to the sentiment_analyzer function and store the response 
+    # Pass the text to the sentiment_analyzer function and store the response
     response = emotion_detection(text_to_analyze)
-    
 
     # Extracting emotions and dominant emotion from response
-    anger = response['anger']
-    disgust = response['disgust']
-    fear = response['fear']
-    joy = response['joy']
-    sadness = response['sadness']
-    dominant_emotion = response['dominant_emotion']
+    anger = response["anger"]
+    disgust = response["disgust"]
+    fear = response["fear"]
+    joy = response["joy"]
+    sadness = response["sadness"]
+    dominant_emotion = response["dominant_emotion"]
 
     # Formatting the output string
     return (
@@ -34,15 +33,16 @@ def emotionDetector():
         f"'sadness': {sadness}. The dominant emotion is {dominant_emotion}."
     )
 
+
 @app.route("/")
 def render_index_page():
-    ''' This function initiates the rendering of the main application
-        page over the Flask channel
-    '''
-    return render_template('index.html')
+    """This function initiates the rendering of the main application
+    page over the Flask channel
+    """
+    return render_template("index.html")
+
 
 if __name__ == "__main__":
-    ''' This functions executes the flask app and deploys it on localhost:5000
-    '''
+    """ This functions executes the flask app and deploys it on localhost:5000
+    """
     app.run(debug=True)
-
